e5d6ffa6d5c9db4a52bb423df16c2cd8f9f2ca0e
Sameer Goyal <sam007.goyal@gmail.com>
2013-02-04 00:19:38 +0530

    Basic functionality added

--- js/main.js
+++ js/main.js
@@ -3,9 +3,10 @@ var context;
 var pixelWidth = 800;
 var pixelHeight = 600;
 var borderStyle = "thick solid";
-var player = {x:10,y:10,dx:1,dy:0};
+var player = {x:10,y:10,angle:0};
 var constants = {LEFT:37,UP:38,RIGHT:39,DOWN:40};
-
+var speed = 2;
+var angularSpeed = 0.1;
 /*
 Called on each pressed key
 */
@@ -14,31 +15,33 @@ function input(e)
 	switch(e.keyCode)
 	{
 		case constants.UP:
-		player.x+=player.dx;
-		player.y+=player.dy;
+		player.x+=speed*Math.cos(player.angle);
+		player.y+=speed*Math.sin(player.angle);
 		break;
 		case constants.DOWN:
-
+		player.x-=speed*Math.cos(player.angle);
+		player.y-=speed*Math.sin(player.angle);
 		break;
 		case constants.RIGHT:
-
+		player.angle+=angularSpeed;
 		break;
 		case constants.LEFT:
-
+		player.angle-=angularSpeed;
 		break;
 	}
 }
 
 function draw()
 {
-	
+	context.clearRect(0,0,pixelWidth,pixelHeight);
+	context.fillRect(player.x-5,player.y-5,10,10);
 }
 
 function newGame()
 {
 	//TODO generate a grid & initial position of for now only 1 player.
-	draw();
 	document.addEventListener("keypress", input);
+	setInterval(draw,10);
 }
 
 function initGame()
