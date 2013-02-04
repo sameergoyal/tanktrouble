085fd99d68a4e7e9999136269100ee4c74478a20
Sameer Goyal <sam007.goyal@gmail.com>
2013-02-04 21:23:33 +0530

    Paper.js included

--- js/main.js
+++ js/main.js
@@ -3,10 +3,14 @@ var context;
 var pixelWidth = 800;
 var pixelHeight = 600;
 var borderStyle = "thick solid";
-var player = {x:10,y:10,angle:0};
+var player = {x:30,y:30,oldAngle:0,newAngle:0};
 var constants = {LEFT:37,UP:38,RIGHT:39,DOWN:40};
 var speed = 2;
-var angularSpeed = 0.1;
+var angularSpeed = 2;
+var playerTank;
+
+paper.install(window);
+
 /*
 Called on each pressed key
 */
@@ -15,43 +19,46 @@ function input(e)
 	switch(e.keyCode)
 	{
 		case constants.UP:
-		player.x+=speed*Math.cos(player.angle);
-		player.y+=speed*Math.sin(player.angle);
+		player.x+=speed*Math.cos(player.newAngle*(Math.PI/180));
+		player.y+=speed*Math.sin(player.newAngle*(Math.PI/180));
 		break;
 		case constants.DOWN:
-		player.x-=speed*Math.cos(player.angle);
-		player.y-=speed*Math.sin(player.angle);
+		player.x-=speed*Math.cos(player.newAngle*(Math.PI/180));
+		player.y-=speed*Math.sin(player.newAngle*(Math.PI/180));
 		break;
 		case constants.RIGHT:
-		player.angle+=angularSpeed;
+		player.newAngle+=angularSpeed;
 		break;
 		case constants.LEFT:
-		player.angle-=angularSpeed;
+		player.newAngle-=angularSpeed;
 		break;
 	}
 }
 
 function draw()
 {
-	context.clearRect(0,0,pixelWidth,pixelHeight);
-	context.fillRect(player.x-5,player.y-5,10,10);
+	playerTank.position = new Point(player.x,player.y);
+	playerTank.rotate(player.newAngle-player.oldAngle);
+	player.oldAngle = player.newAngle;
+	document.getElementById('status').innerHTML = player.oldAngle;
 }
 
 function newGame()
 {
 	//TODO generate a grid & initial position of for now only 1 player.
+	playerTank = new Path.Rectangle([player.x-20, player.y-10], [40, 20]);
+    playerTank.strokeColor = 'black';
 	document.addEventListener("keypress", input);
-	setInterval(draw,10);
+	view.onFrame = draw;
 }
 
-function initGame()
+window.onload = function initGame()
 {
 	if(!arena)
 	{
-		arena = document.createElement("canvas");
-		arena.id = "arena";
-		document.body.appendChild(arena);
+		arena = document.getElementById('arena');
 	}
+	paper.setup(arena);
 	context = arena.getContext("2d");
 	arena.width = pixelWidth;
 	arena.height = pixelHeight;
